63c98c5e97b0092f81a4c9797f526d46e5224567
MacBobby Chibuzor <theghostmac@gmail.com>
2022-03-18 19:24:26 -0400

    added apply method

--- numberSolver.hs
+++ numberSolver.hs
@@ -15,4 +15,11 @@ instance Show Op where
 valid :: Op -> Int -> Int -> Bool
 valid Add _ _ = True
 valid Sub x y = x > y
-"numberSolver.hs" 181L, 5330C
+valid Mul _ _ = True
+valid Div x y = x `mod` y == 0
+
+apply :: Op -> Int -> Int -> Int
+apply Add x y = x + y
+apply Sub x y = x - y
+apply Mul x y = x * y
+apply Div x y = x `div` y
