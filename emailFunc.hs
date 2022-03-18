3a2c23ae8335657a53d2dffd6c1b02d23af04682
MacBobby Chibuzor <83423188+theghostmac@users.noreply.github.com>
2022-03-18 16:09:06 +0100

    update emailFunc.hs

--- emailFunc.hs
+++ emailFunc.hs
@@ -1,2 +1,5 @@
 mailTo recipient = "Dear " ++ recipient ++ ",\n"
-messageContent body = "It is so nice to hear from you again!"
+mailContent body = "It is so nice to hear from you again!" ++ body ++ "! Bye."
+mailFrom me = "Best Regards, \n MacBobby."
+
+replyMail recipient body me = mailTo recipient ++ mailContent body ++ mailFrom me
