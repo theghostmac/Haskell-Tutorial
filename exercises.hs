1dbc960083d230d670eab551ca5e8ddb762c7f7b
Ghost Mac <theghostmac@gmail.com>
2022-04-04 09:44:20 -0400

    exercise 2

--- exercises.hs
+++ exercises.hs
@@ -4,3 +4,7 @@ last [1,2,3,4]
 last xs = xs !! (length xs - 1)
 -- or reverse and pick head
 last xs = head(reverse xs)
+-- 2: give everything asides the last element
+init [1,2,3,4]
+init ys = take(length ys -1)
+init ys = reverse(tail(reverse ys))
