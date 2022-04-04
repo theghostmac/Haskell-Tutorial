0183f04750c629ebc9a25bd1a05a64f7f5261100
Ghost Mac <theghostmac@gmail.com>
2022-04-04 14:05:16 -0400

    review data types and structures indepth

--- SoH.hs
+++ SoH.hs
@@ -24,11 +24,25 @@ a = [1,2,3,4,5] :: [Int]
 b = [False, True, False, False] :: [Bool]
 c = ["one","two","three"] :: [String]
 d = [['a', 'b', 'c'], ['d', 'e', 'f']] :: [[Char]]
+values :: [Int]
+values = [1,2,3]
+
+testScores :: [Double]
+testScores = [99.5,65.3,80.22]
+
+moreLetters :: [Char] -- Char is synonymous as String
+moreLetters = ['a','b','k']
+evenMoreLetters :: [String]
+evenMoreLetters = ["abc","def", "ghi"]
+
 -- Tuples
 -- tuples can have different types, makes use of 
 -- parentheses
 e = (False, True) :: (Bool, Bool)
 f = ("Yes", 'a', True) :: (String, Char, Bool)
+streetAddress :: (Int, String)
+streetAddress = (10, "Hilltop Hostel Str.")
+
 
 -- Function types
 -- function is a mapping from arguments of one type
